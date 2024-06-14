857d89a1170f9f036d69e01e1967fc2c52aa78fa
Ishani Sangal <ishani@Ishanis-Laptop.local>
2024-06-14 16:49:33 +0530

    Your commit message

--- app.py
+++ app.py
@@ -1,12 +1,12 @@
 from flask import Flask, render_template, request
-from backend import response
+from backend import response                    #to handle chat responses
 
 
-app = Flask(__name__)
+app = Flask(__name__)                           #creates an instance of flask class to run the web application
 
 @app.route('/')
-def index():
-    return render_template('index.html')
+def index():                                    #handles requests to root url
+    return render_template('index.html')        #renders the html template
 
 #def chat():
     #user_url = request.form.get('url')
@@ -17,12 +17,12 @@ def index():
     #return jsonify({"answer": answer})
 
 
-@app.route('/chat', methods=['GET', 'POST']) 
+@app.route('/chat', methods=['GET', 'POST'])     #defines a route to the chat url that accepts both requests
 def chat():
-    message = request.form['msg'] 
-    return response(message) 
+    message = request.form['msg']               #retrieves the message parameter from the incoming data
+    return response(message)                    #calls the response function from the backend file
 
 
 
 if __name__ == '__main__':
-    app.run() 
+    app.run()                                  #starts the flask server and checks if script is running directly
